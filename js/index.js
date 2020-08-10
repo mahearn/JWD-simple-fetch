6e1ba8bb16a12bda3501f64b7dd8cc07e6a50a3a
Michael Ahearn <mahearnpad@gmail.com>
2020-08-11 08:08:38 +1000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DomContentLoaded', function () {
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+});
